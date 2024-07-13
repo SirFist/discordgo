d1a0730b7099c036de485943e4a7197973918863
SirFist <3142245+SirFist@users.noreply.github.com>
2024-07-13 19:06:48 +0100

    Summary (Required)

--- examples/echo/main.go
+++ examples/echo/main.go
@@ -7,7 +7,7 @@ import (
 	"os/signal"
 	"strings"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/sirfist/discordgo"
 )
 
 type optionMap = map[string]*discordgo.ApplicationCommandInteractionDataOption
